3f81370280cfc859b9abf0b31db60b0ebcb863ee
Victoria Bobak <victoriab.06@icloud.com>
2020-09-19 22:04:13 +0100

    Initial Commit

---
+++ Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Victoria Bobak on 19/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
